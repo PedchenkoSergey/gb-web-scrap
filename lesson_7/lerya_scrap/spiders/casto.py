1e2b697af90fd3f29884daab663a8a9ac20ff450
spedchenko <se.pedchenko@yandex.ru>
2022-05-04 16:00:03 +0300

    Finished homework: itemloader, separate folders, additional stats

--- lesson_7/lerya_scrap/spiders/casto.py
+++ lesson_7/lerya_scrap/spiders/casto.py
@@ -17,14 +17,25 @@ class CastoSpider(scrapy.Spider):
     def parse(self, response: HtmlResponse, **kwargs):
         print("!!!CASTO START!!!")
         print(f"Casto response: {response.status}")
+        next_page = response.xpath("//a[@class='next i-next']/@href").get()
+        if next_page:
+            yield response.follow(next_page, callback=self.parse)
+
         links = response.xpath("//a[@class='product-card__name ga-product-card-name']")
         for link in links:
-            yield response.follow(link, callback=self.parse_ads)
+            yield response.follow(link, callback=self.parse_goods)
 
-    def parse_ads(self, response: HtmlResponse):
+    def parse_goods(self, response: HtmlResponse):
         loader = ItemLoader(item=CastoScrapItem(), response=response)
         loader.add_xpath('name', "//h1[contains(@class, 'product-essential__name')]/text()")
-        loader.add_xpath('price', "//span[@class='regular-price']/text()")
-        loader.add_xpath('photos', "//img[contains(@class, 'top-slide__img')]/@src")
+        loader.add_xpath('price', "//span[@class='regular-price']/span/span/span[1]/text()")
+        loader.add_xpath('photos', "//img[contains(@class, 'top-slide__img')]/@data-src")
+        loader.add_xpath('stats', "//div[contains(@class, 'product-specifications')]/dl")
         loader.add_value('url', response.url)
         yield loader.load_item()
+
+        # name = response.xpath("//h1[contains(@class, 'product-essential__name')]/text()").get()
+        # price = response.xpath("//span[@class='regular-price']/span/span/span[1]/text()").get()
+        # photos = response.xpath("//img[contains(@class, 'top-slide__img')]/@data-src").getall()
+        # url = response.url
+        # yield CastoScrapItem(name=name, price=price, photos=photos, url=url)
